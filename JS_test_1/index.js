fa16e781e733b7fad387905872389337ebf08453
jonasmouret <jonas@mouret.org>
2020-07-26 19:34:20 +0200

    End of the exercise

---
+++ JS_test_1/index.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "inline-block";
+});
+
+
+document.getElementById("closeModal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
